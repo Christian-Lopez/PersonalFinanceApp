5eb7204b25dd09f69f25d66ea76c7d4cf9b3d4a5
Christian Lopez <cjavier93@gmail.com>
2026-09-05 21:19:20 -0600

    chore: add localhost:5174 to allowed CORS origins

--- PersonalFinanceApp.Api/Program.cs
+++ PersonalFinanceApp.Api/Program.cs
@@ -89,7 +89,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:5173")
+            policy.WithOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:5173", "http://localhost:5174")
                   .AllowAnyHeader()
                   .AllowAnyMethod()
                   .AllowCredentials();
