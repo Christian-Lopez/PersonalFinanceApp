8a9b3a27d91c58521ec98ff5c52c92432e432454
Christian Lopez <cjavier93@gmail.com>
2026-09-12 23:25:44 -0600

    fix: allow any origin for CORS to support cloud deployment

--- PersonalFinanceApp.Api/Program.cs
+++ PersonalFinanceApp.Api/Program.cs
@@ -97,10 +97,9 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:5173", "http://localhost:5174")
+            policy.AllowAnyOrigin()
                   .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
+                  .AllowAnyMethod();
         });
 });
 
